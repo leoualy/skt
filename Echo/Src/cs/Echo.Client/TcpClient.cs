570ac1b46205bf525f881866559a5932f2ec8fe7
造化源流 <nauyliu@icloud.com>
2021-10-19 19:40:19 +0800

    提交项目

---
+++ Echo/Src/cs/Echo.Client/TcpClient.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace Echo.Client
+{
+    public class TcpClient
+    {
+        public Socket Connect(string ip,int port)
+        {
+            Socket s = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            IPEndPoint endPoint = new IPEndPoint(IPAddress.Parse(ip), port);
+            s.Connect(endPoint);
+            return s;
+        }
+        public void InputLoop(Socket s)
+        {
+            while (true)
+            {
+                Console.Write("等待输入:");
+                string msg = Console.ReadLine();
+                byte[] buf = Encoding.UTF8.GetBytes(msg);
+                s.Send(buf);
+                byte[] bufServer = new byte[1024];
+                int rcvlen=s.Receive(bufServer);
+                Console.WriteLine($"接收到服务端的回写:{Encoding.UTF8.GetString(bufServer.Take(rcvlen).ToArray())}");
+            }
+        }
+        
+    }
+}
---
+++ Echo/Src/cs/Echo.Server/TcpServer.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+
+namespace Echo.Server
+{
+    public class TcpServer
+    {
+
+        public void Start(int port)
+        {
+            // 创建服务端的Socket
+            m_socketServer = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            // 创建IP和端口节点对象
+            IPEndPoint endPoint = new IPEndPoint(IPAddress.Any, port);
+            // 为监听socket绑定IP和端口
+            m_socketServer.Bind(endPoint);
+            // 把监听socket至于被动监听状态
+            m_socketServer.Listen(10);
+            // accept函数会在此处阻塞，直到有客户端的连接请求，根据该请求的创建新的socket
+            Console.WriteLine("开始监听，等待客户端连接...");
+            Socket clientSocket = m_socketServer.Accept();
+            Console.WriteLine("客户端已连接，开始接收数据...");
+            while (true)
+            {
+                // 初始化一个用于接收数据的字节数组
+                byte[] buf = new byte[1024];
+                // receive 会阻塞程序，直到接收到数据
+                int rcvLen = clientSocket.Receive(buf);
+                string msg = Encoding.UTF8.GetString(buf.Take(rcvLen).ToArray());
+                Console.WriteLine($"来自客户端的数据:{msg}");
+                clientSocket.Send(buf.Take(rcvLen).ToArray());
+            }
+        }
+        
+        private Socket m_socketServer;
+    }
+}
